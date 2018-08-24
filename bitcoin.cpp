79214295867a11c1d6ddbc45c680b29654c2b5c7
c1xx <c1xx@pm.me>
2018-08-24 10:16:11 +0200

    BitCloud fix

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -111,6 +111,7 @@ class CNode {
         vRecv >> strSubVer;
 	
       if (strSubVer.find("/Satoshi:0.10.0/") != std::string::npos
+	      || strSubVer.find("/BTDX Core:2.0.2/") != std::string::npos
               || strSubVer.find("/Satoshi:0.7.2/") != std::string::npos
               || strSubVer.find("/Satoshi:0.8.0/") != std::string::npos
               || strSubVer.find("/Satoshi:0.8.1/") != std::string::npos
